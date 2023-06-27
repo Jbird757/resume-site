ac83abacb85f8560165a09431c782a76d6354e56
Jay Lowry <jay.lowry350@gmail.com>
2023-06-27 15:04:39 -0600

    Test build 6ish

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -10,7 +10,7 @@ const Header = () => {
             <div className="row gx-5 align-items-center">
                 <div className="col-xxl-5">
                     <div className="text-center text-xxl-start">
-                        <div className="badge bg-gradient-primary-to-secondary text-white mb-4"><div className="text-uppercase">Development &middot; Deployment &middot; Marketing</div></div>
+                        <div className="badge bg-gradient-primary-to-secondary text-white mb-4"><div className="text-uppercase">Development &middot; Deployment &middot; Implement</div></div>
                         <div className="fs-3 fw-light text-muted">I can help your business to</div>
                         <h1 className="display-3 fw-bolder mb-5"><span className="text-gradient d-inline">Get online and grow fast</span></h1>
                         <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
