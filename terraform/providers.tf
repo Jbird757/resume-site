e111f9af46087dfc265c8512c45e5e17daaacba2
Jay Lowry <jay.lowry350@gmail.com>
2024-03-17 21:38:32 -0600

    created new actions pipeline, added a provider

---
+++ terraform/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.40.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         	    = "personal-site-tf-backend"
+    key                 = "state/terraform.tfstate"
+    region         	    = "us-east-1"
+    encrypt        	    = true
+    dynamodb_table      = "personal-site-tf-lock"
+  }
+}
