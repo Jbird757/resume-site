b43141b826199b8a54ac63072b4002e700d007e4
Jay Lowry <jay.lowry350@gmail.com>
2024-03-30 17:19:01 -0600

    testing copy 5

--- terraform/modules/s3-cloudfront/io.tf
+++ terraform/modules/s3-cloudfront/io.tf
@@ -24,6 +24,6 @@ variable "alternate_domains" {
 
 #--------------------------------------- Inputs to the Module ---------------------------------------#
 
-output "bucket_name" {
+output "bucket_arn" {
   value = aws_s3_bucket.site_build.arn
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,12 +2,12 @@ locals {
   common_tags = {
     app = "personal-site"
   }
-  bucket_name = "jl-personal-site"
+  bucket_name = "jel-personal-site"
   domain = "jaylowry.com"
   alt_domains = ["www.jaylowry.com"]
   aws-region = "us-east-1"
 }
 
-output module_bucket_name {
-  value = module.personal-site.bucket_name
+output module_bucket_arn {
+  value = module.personal-site.bucket_arn
 }
