7cfb9f3d445eadcce09fa25714ce18462317c237
Jay Lowry <jay.lowry350@gmail.com>
2024-04-05 20:36:43 -0600

    updated more of the site info

--- src/components/ProjectContents.js
+++ src/components/ProjectContents.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import GPTLogo from '../assets/ChatGPT_logo.svg'
+import tflogo from '../assets/terraform.png'
 import Homelab from '../assets/homelab.jpg'
 import Website from '../assets/This website2.png'
 import SchoolProject from '../assets/School Project.png'
@@ -22,7 +23,7 @@ const Contents = () => {
                                             <h2 className="fw-bolder" href="https://github.com/Jbird757/resume-site">This Website   <a className="text-gradient" href="https://github.com/Jbird757/resume-site"><i className="bi bi-github"></i></a></h2>
                                             <p>
                                                 I made this website both to be a companion to my resume and to showcase much of what I have learned during my years in school and in the workforce. The goal for this website was not only to create 
-                                                a website, but to do it in a way that used professional best practices in hosting, security, and redundancy. This site is hosted serverlessly on AWS, and previous versions of the site were hosted on a 
+                                                a website, but to do it in a way that used professional-grade best practices in hosting, security, and redundancy. This site is hosted serverlessly on AWS, and previous versions of the site were hosted on a 
                                                 configured virtual machine. This website is completely automated using Terraform and GitHub Actions. Technologies that I learned or expanded my knowledge of as part of this project: React, GitHub Actions, 
                                                 Terraform, serverless web services, AWS and AWS CLI, security through OIDC, firewall and web server management, and more. Click on the GitHub logo to view the repository.
                                             </p>
@@ -40,10 +41,10 @@ const Contents = () => {
                                             <p>
                                                 My next project is a Terraform Module that sets up an entire serverless website on AWS complete with encryption and security best practices. I noticed when I was building my personal site that I couldn't find a 
                                                 module that fit what I needed to do, especially since S3 Bucket ACLs are deprecated and CloudFront OAC is the new standard, so I decided to make my own. I hope that others besides me can benefit from this module, 
-                                                so feel free to check it out at the GitHub link above.
+                                                and I would love it if you checked it out at the GitHub link above.
                                             </p>
                                         </div>
-                                        <img className="img-fluid project-picture" src={ GPTLogo } alt="ChatGPT Logo" />
+                                        <img className="img-fluid project-picture" src={ tflogo } alt="Terraform Logo" />
                                     </div>
                                 </div>
                             </div>
@@ -53,9 +54,9 @@ const Contents = () => {
                                         <div className="p-5">
                                             <h2 className="fw-bolder">School Projects   <a className="text-gradient" href="https://github.com/Jbird757"><i className="bi bi-github"></i></a></h2>
                                             <p>
-                                                During my time as a college student, I have had the opportunity to take a variety of classes, including 3 web development classes where I 
-                                                learned Python/Django, C#/ASP.net, and Vue. I have had experience with database design and technology, and extensive experience with 
-                                                lower-level language coding in C and C++ to create services like parsers and data structures.
+                                                During my time as a college student, I have had the opportunity to take a variety of classes, including classes in web development, cybersecurity, AWS and 
+                                                cloud management, and much more. While not all of these classes included projectson GitHub, many did, so feel free to look around and explore the things that I've done 
+                                                all the way to my graduate studies (more impressive) from freshman year (much less impressive).
                                             </p>
                                         </div>
                                         <img className="img-fluid project-picture" src={ SchoolProject } alt="Colab Notebook" />
@@ -70,8 +71,8 @@ const Contents = () => {
                                             <p>
                                                 In addition to the above projects, I have a project that is ongoing and will forever be ongoing. I manage my home network and run several 
                                                 machines that I built myself for different purposes. Currently I have set up a file server running TrueNAS Scale, and a "compute" or systems 
-                                                server running several VMs and Docker Containers for services like PiHole and monitoring and updating services like WatchTower and Portainer. 
-                                                I plan to expand this in the future to include many more useful services and eventually take most of my data out of the cloud.
+                                                server running several VMs and Docker containers for services like PiHole and monitoring and updating services like WatchTower and Portainer. 
+                                                I plan to expand this in the future to include many more useful services and eventually take most of my personal data out of the cloud.
                                             </p>
                                         </div>
                                         <img className="img-fluid project-picture" src={ Homelab } alt="My File Server" />
